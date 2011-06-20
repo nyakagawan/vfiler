ed4fbcf8935614e70439869eb5df58932ce428a8
中川 均 <nak@nak-mac.local>
2011-06-21 02:30:49 +0900

    first commit

---
+++ define.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Def:
+    CORSOR_UP_KEYCODE = ord("k")
+    CORSOR_DOWN_KEYCODE = ord("j")
+    CORSOR_LEFT_KEYCODE = ord("h")
+    CORSOR_RIGHT_KEYCODE = ord("l")
+
+    FILE_EDIT_KEYCODE = ord("e")
+
+    LIST_COL_INDEX_NAME = 0
+    LIST_COL_INDEX_EXT = 1
+    LIST_COL_INDEX_SIZE = 2
+    LIST_COL_INDEX_MTIME = 3
+
+    MTIME_FORMAT = '%Y-%m-%d %H:%M'
+
