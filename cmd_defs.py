0b22096f0547a7ede3130cb663e70586db8b7588
nyakagawan <isoidemasu@gmail.com>
2011-07-30 16:39:26 +0900

    追加漏れ

---
+++ cmd_defs.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import wx
+
+from define import Def
+from util import Util
+from commander import Commander, CommandDef
+from listCtrl import ListCtrl
+from textCtrl import TextCtrl
+
+def Cmd_CursorUp( vfiler, event ):
+    listCtrl = vfiler.getFocusedPane()
+    listCtrl.moveCursor( ListCtrl.MOVE_CURSOR_UP )
+
+def Cmd_CursorDown( vfiler, event ):
+    listCtrl = vfiler.getFocusedPane()
+    listCtrl.moveCursor( ListCtrl.MOVE_CURSOR_DOWN )
+
+def Cmd_CursorLeft( vfiler, event ):
+    listCtrl = vfiler.getFocusedPane()
+    listCtrl.changeFocus( Def.PANE_KIND_LEFT )
+
+def Cmd_CursorRight( vfiler, event ):
+    listCtrl = vfiler.getFocusedPane()
+    listCtrl.changeFocus( Def.PANE_KIND_RIGHT )
+
+def Cmd_MoveDirUp( vfiler, event ):
+    listCtrl = vfiler.getFocusedPane()
+    listCtrl.moveDir( ListCtrl.MOVE_DIR_UP )
+
+def Cmd_MoveDirDown( vfiler, event ):
+    listCtrl = vfiler.getFocusedPane()
+    listCtrl.moveDir( ListCtrl.MOVE_DIR_DOWN )
+
+def Cmd_FileEdit( vfiler, event ):
+    listCtrl = vfiler.getFocusedPane()
+    focusedItemIndex = listCtrl.GetFocusedItem()
+    cmd = "mvim --remote-silent %s" %( listCtrl.getItemAbsPath( focusedItemIndex ) )
+    Util.trace("file edit command( %s )" %(cmd) )
+    os.system( cmd )
+
+def Cmd_Quit( vfiler, event ):
+    vfiler.Close()
+
+def Cmd_Copy( vfiler, event ):
+    """ 選択中エレメントを非フォーカスペインのディレクトリへコピーする
+    """
+    listCtrl = vfiler.getFocusedPane()
+    focusedItemIndex = listCtrl.GetFocusedItem()
+    if listCtrl.getElemCount() and focusedItemIndex>=0:
+        forcusedElem = listCtrl.getElem( focusedItemIndex )
+        srcPath = forcusedElem.getAbsPath()
+        destPath = None
+        unfocusedPane = vfiler.getUnFocusedPane()
+        if unfocusedPane:
+            destPath = unfocusedPane.getCurDir()
+        if destPath and destPath!=os.path.dirname( srcPath ):
+            cmd = "cp -rf %s %s" %( srcPath, destPath )
+            Util.trace( cmd )
+            os.system( cmd )
+
+def Cmd_Move( vfiler, event ):
+    Util.trace( "NOT IMPLEMENT!" )
+
+def Cmd_Delete( vfiler, event ):
+    """ 選択中エレメントをファイルリストから削除する
+    """
+    listCtrl = vfiler.getFocusedPane()
+    focusedItemIndex = listCtrl.GetFocusedItem()
+    if listCtrl.getElemCount() and focusedItemIndex>=0:
+        forcusedElem = listCtrl.getElem( focusedItemIndex )
+        # elemListから削除
+        listCtrl.elemList.remove( forcusedElem )
+        # GUIから削除
+        listCtrl.DeleteItem( focusedItemIndex )
+        # FileSystemから削除
+        cmd = "rm -rf %s" %( forcusedElem.getAbsPath() )
+        Util.trace( cmd )
+        os.system( cmd )
+
+def Cmd_Search( vfiler, event ):
+    """ インクリメンタルサーチ
+    """
+    vfiler.focusSearchTextCtrl()
+
+def Cmd_Grep( vfiler, event ):
+    """ Grep
+    """
+    vfiler.focusGrepTextCtrl()
+
+def Cmd_SameDir( vfiler, event ):
+    """ フォーカスのないリストのディレクトリを、フォーカスのあるリストと一緒にする
+    """
+    listCtrl = vfiler.getFocusedPane()
+    dirToChange = listCtrl.getCurDir()
+    vfiler.getUnFocusedPane().changeDir( dirToChange )
+
+
+
+
---
+++ keyMapper.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import wx
+
+from define import Def
+
+class KeyDef():
+    """ Key定義。
+        KeyCode（ord("a")とか）とModifier定義（MOD_KEY_XXXX）からなる
+    """
+    MOD_KEY_ALT     =(1<<0)
+    MOD_KEY_CMD     =(1<<1)
+    MOD_KEY_CTRL    =(1<<2)
+    MOD_KEY_SHIFT   =(1<<3)
+
+    def __init__( self, keycode, modKey=0 ):
+        self.keycode = keycode
+        self.modKey = modKey
+
+    def getKey( self ):
+        """ KeyHashを取得
+            Command辞書のKeyとなる
+        """
+        return "m_%d_c_%d" %(self.modKey, self.keycode)
+
+    @classmethod
+    def getKeyFromEvent( cls, event ):
+        """ KeyHashを取得
+            KeyDownイベントオブジェクトから作って返す
+        """
+        keycode = event.GetKeyCode()
+        modKey = 0
+        if event.AltDown():    modKey |= cls.MOD_KEY_ALT
+        if event.CmdDown():    modKey |= cls.MOD_KEY_CMD
+        if event.ControlDown():modKey |= cls.MOD_KEY_CTRL
+        if event.ShiftDown():  modKey |= cls.MOD_KEY_SHIFT
+        return cls( keycode, modKey ).getKey()
+
+class CommandDef():
+    """ Command定義。KeyDefとCommand関数からなる
+    """
+    def __init__( self, keyDef, cmdFunc ):
+        self.keyDef = keyDef
+        self.cmdFunc = cmdFunc
+    def getKey( self ):
+        return self.keyDef.getKey()
+    def getCmd( self ):
+        return self.cmdFunc
+
+
+class KeyMapper():
+    vfiler = None
+    cmdDict = {}    # KeyはKeyDefのKeyHash。ValueはCommand関数
+    cancelKeyDefs = [
+            KeyDef( ord("["), KeyDef.MOD_KEY_CTRL ).getKey(),
+            KeyDef( wx.WXK_ESCAPE ).getKey(),
+            ]
+    decideKeyDefs = [
+            KeyDef( wx.WXK_RETURN ).getKey(),
+            ]
+
+    @classmethod
+    def add( cls, cmdDef ):
+        """ KeyMappingの追加
+        """
+        cls.cmdDict[ cmdDef.getKey() ] = cmdDef.getCmd()
+
+    @classmethod
+    def get( cls, key ):
+        """ KeyHashからCommandの取得
+        """
+        if cls.cmdDict.has_key( key ):
+            return cls.cmdDict[ key ]
+        return None
+
+    @classmethod
+    def execute( cls, event ):
+        """ KeyにMappingされたCommandの実行
+        """
+        key = KeyDef.getKeyFromEvent( event )
+        cmd = cls.get( key )
+        if cmd:
+            cmd( cls.vfiler, event )
+
+    @classmethod
+    def isCancel( cls, event ):
+        key = KeyDef.getKeyFromEvent( event )
+        for cancelKeyDef in cls.cancelKeyDefs:
+            if key==cancelKeyDef:
+                return True
+        return False
+
+    @classmethod
+    def isDecide( cls, event ):
+        key = KeyDef.getKeyFromEvent( event )
+        for decideKeyDef in cls.decideKeyDefs:
+            if key==decideKeyDef:
+                return True
+        return False
+
+
+
+class KeyMapper_ListCtrl( KeyMapper ):
+    """ ListCtrlのKeyMapper
+    """
+    @classmethod
+    def setup( cls, vfiler ):
+        cls.vfiler = vfiler
+
+        from cmd_defs import *
+        cls.add( CommandDef( KeyDef( ord("K") ), Cmd_CursorUp ) )
+        cls.add( CommandDef( KeyDef( ord("J") ), Cmd_CursorDown ) )
+        cls.add( CommandDef( KeyDef( ord("H") ), Cmd_CursorLeft ) )
+        cls.add( CommandDef( KeyDef( ord("L") ), Cmd_CursorRight ) )
+
+        cls.add( CommandDef( KeyDef( ord("U") ), Cmd_MoveDirUp ) )
+        cls.add( CommandDef( KeyDef( ord("D") ), Cmd_MoveDirDown ) )
+
+        cls.add( CommandDef( KeyDef( ord("E") ), Cmd_FileEdit ) )
+        cls.add( CommandDef( KeyDef( ord("Q") ), Cmd_Quit ) )
+        cls.add( CommandDef( KeyDef( ord("C") ), Cmd_Copy ) )
+        cls.add( CommandDef( KeyDef( ord("M") ), Cmd_Move ) )
+        #cls.add( CommandDef( KeyDef( ord("D") ), Cmd_Delete ) )
+        cls.add( CommandDef( KeyDef( ord("/") ), Cmd_Search ) )
+        cls.add( CommandDef( KeyDef( ord("G") ), Cmd_Grep ) )
+        cls.add( CommandDef( KeyDef( ord("O") ), Cmd_SameDir ) )
+
+
+class KeyMapper_TextCtrl( KeyMapper ):
+    """ TextCtrlのKeyMapper
+    """
+    pass
+
+
